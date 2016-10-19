39b74c05d281259a4a4ef3afa14028808b92d662
hennry <hennry@126.com>
2016-10-19 09:20:11 +0800

    add exit dialog

--- src/com/example/viewtest/MainActivity.java
+++ src/com/example/viewtest/MainActivity.java
@@ -24,11 +24,14 @@ import java.util.List;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -316,6 +319,48 @@ public  class MainActivity extends Activity  {
 		
 		return dateTime;
 	}
+	
+	@Override  
+    public boolean onKeyDown(int keyCode, KeyEvent event)  
+    {  
+        if (keyCode == KeyEvent.KEYCODE_BACK )  
+        {  
+            // 创建退出对话框  
+            AlertDialog isExit = new AlertDialog.Builder(this).create();  
+            // 设置对话框标题  
+            isExit.setTitle("系统提示");  
+            // 设置对话框消息  
+            isExit.setMessage("确定要退出吗");  
+            // 添加选择按钮并注册监听  
+            isExit.setButton("确定", listener);  
+            isExit.setButton2("取消", listener);  
+            // 显示对话框  
+            isExit.show();  
+  
+        }  
+          
+        return false;  
+          
+    }  
+	
+    /**监听对话框里面的button点击事件*/  
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener()  
+    {  
+        public void onClick(DialogInterface dialog, int which)  
+        {  
+            switch (which)  
+            {  
+            case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序  
+                finish();  
+                break;  
+            case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框  
+                break;  
+            default:  
+                break;  
+            }  
+        }  
+    }; 
+	
 	
     //这里横竖屏切换是横屏和竖屏采用相同布局
     @Override
